804eda06d17c40bf3c95a1f6d18cab2583f36876
Robin Sallis <Toutouwai@users.noreply.github.com>
2018-11-22 19:16:21 +1300

    editorconfig

--- TemplateFieldWidths.js
+++ TemplateFieldWidths.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('#Inputfield_inputfield_widths input').on('change', function(event) {
+	$('#Inputfield_inputfield_widths input').on('change', function() {
 		var width = $(this).val();
 		$(this).closest('.tfw-item').css('width', width + '%');
 	});
